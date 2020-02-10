334a58267e6f9d8373194108f20ebc0fbcf0a7bf
owowow <43901551+owowa@users.noreply.github.com>
2020-02-10 22:04:15 +0900

    0210

--- app/src/main/java/com/example/proto2/LeftFragment.java
+++ app/src/main/java/com/example/proto2/LeftFragment.java
@@ -1,12 +1,20 @@
 package com.example.proto2;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class LeftFragment extends Fragment {
@@ -15,7 +23,7 @@ public class LeftFragment extends Fragment {
 
     private String title;
     private int page;
-
+    private Button TestBtn;
 
     public static LeftFragment newInstance(String param1, String param2) {
         LeftFragment fragment = new LeftFragment();
@@ -31,12 +39,50 @@ public class LeftFragment extends Fragment {
         super.onCreate(savedInstanceState);
         page = getArguments().getInt("someInt", 0);
         title = getArguments().getString("someTitle");
-
+        new HttpAsyncTask().execute("http://goo.gl/eIXu9l");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_left, container, false);
+        TestBtn = (Button) view.findViewById(R.id.button);
+        TestBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return view;
     }
+
+    private static class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        OkHttpClient client = new OkHttpClient();
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = null;
+            String strUrl = params[0];
+
+            try{
+                Request req = new Request.Builder().url(strUrl).build();
+                Response res = client.newCall(req).execute();
+                result = res.body().string();
+                Log.d("----HTTP", result);
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s != null){
+                Log.d("----HTTP", s);
+            }
+        }
+    }
+
+
 }
