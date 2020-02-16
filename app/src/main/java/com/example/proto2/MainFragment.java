e70ce422b98f28539348dd2956fd13b797533ccf
owowow <43901551+owowa@users.noreply.github.com>
2020-02-16 18:13:34 +0900

    리사이클러뷰 수정

--- app/src/main/java/com/example/proto2/MainFragment.java
+++ app/src/main/java/com/example/proto2/MainFragment.java
@@ -3,27 +3,34 @@ package com.example.proto2;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Handler;
+import android.os.Message;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -39,6 +46,8 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 
 
 public class MainFragment extends Fragment {
@@ -59,8 +68,21 @@ public class MainFragment extends Fragment {
     private TMapPoint tPoint, currentpoint, endpoint;
     private TMapMarkerItem tItem;
 
-    private Button searchBtn;
-    private TextView descTextview;
+    private Button searchBtn, buttonSearch;
+    private TextView descTextview, speechTextView;
+
+    SpeechRecognizer sRecognizer;
+    Intent i;
+    String key = "";
+
+    TextToSpeech tts;
+
+    final Handler handler = new Handler(){
+        public void handleMessage(Message message){
+            Log.d("----", "핸들러 실행");
+            mAdapter.notifyDataSetChanged();
+        }
+    };
 
     public static MainFragment newInstance(String param1, String param2) {
         MainFragment fragment = new MainFragment();
@@ -86,6 +108,20 @@ public class MainFragment extends Fragment {
         page = getArguments().getInt("someInt", 0);
         title = getArguments().getString("someTitle");
 
+        //음성 인식 intent 설정
+        i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);   //활동 시작
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");   //음성 번역 언어
+        sRecognizer = SpeechRecognizer.createSpeechRecognizer(getContext());    //새 SpeechRecognizer를 만드는 팩토리 메서드
+        sRecognizer.setRecognitionListener(listener);   //리스너 설정
+
+        //TTS 설정
+        tts = new TextToSpeech(getContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                tts.setLanguage(Locale.KOREA);
+            }
+        });
     }
 
     @Override
@@ -107,12 +143,20 @@ public class MainFragment extends Fragment {
         tmapView.setIconVisibility(true);
         tmapView.bringMarkerToFront(tItem);
 
+        buttonSearch = view.findViewById(R.id.buttonSearch);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchDestination();
+
+            }
+        });
+
         searchBtn = view.findViewById(R.id.search_btn);
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                searchDestination();
-                mAdapter.notifyDataSetChanged();
+                speakDestination();
             }
         });
 
@@ -123,18 +167,32 @@ public class MainFragment extends Fragment {
             public void onItemClick(View view, int position) {
                 endpoint = poiNameArr.get(position).getPOI_latlng();
                 Log.d("----TAG", String.valueOf(endpoint));
+                speakTTS(poiNameArr.get(position).getPOI_name()+"을 선택했습니다");
                 GetDirections();
             }
         });
         rv.setAdapter(mAdapter);
 
-        descTextview = (TextView) view.findViewById(R.id.descView);
+//        descTextview = (TextView) view.findViewById(R.id.descView);
+        speechTextView = (TextView) view.findViewById(R.id.speechView);
 
         return view;
     }
 
+    //TTS 말하기
+    public void speakTTS(String sentence) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            String utteranceId=this.hashCode() + "";
+            tts.speak(sentence, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+        } else {
+            HashMap<String, String> hashmap = new HashMap<>();
+            hashmap.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+            tts.speak(sentence, TextToSpeech.QUEUE_FLUSH, hashmap);
+        }
 
-    //현재 위치
+    }
+
+    //현재 위치 리스너
     private final LocationListener mLocationListener = new LocationListener() {
         public void onLocationChanged(Location location) {
             if (location != null) {
@@ -173,7 +231,7 @@ public class MainFragment extends Fragment {
 //        final ArrayList<TMapPoint> pointList = new ArrayList<TMapPoint>();        //검색 마커
 //        pointList.clear();
 
-        String dest = "공원";
+        String dest = "약국";
 
         tData.findAroundNamePOI(currentpoint, dest, 2, 10, new TMapData.FindAroundNamePOIListenerCallback() {
             @Override
@@ -188,11 +246,19 @@ public class MainFragment extends Fragment {
                     poiNameArr.add(data);
 
                     Log.d("----debug----", "\n이름: "+str_name+"\n주소: "+str_addr+"\n좌표: "+poi_point.toString());
+                    speakTTS(str_name);
                 }
+                new Thread(){
+                    public void run(){
+                        Message msg = handler.obtainMessage();
+                        handler.sendMessage(msg);
+                    }
+                }.start();
             }
         });
     }
 
+    //경로 데이터 얻기
     public void GetDirections() {
         final ArrayList<String> descArr = new ArrayList<String>();
         descArr.clear();
@@ -215,6 +281,7 @@ public class MainFragment extends Fragment {
                     }
                 }
                 descTextview.setText(descArr.get(0));
+                speakTTS(descArr.get(0));
             }
 
         });
@@ -228,4 +295,118 @@ public class MainFragment extends Fragment {
             }
         });
     }
+
+    //음성 인식 실행
+    public void speakDestination(){
+        sRecognizer.startListening(i);  //듣기 시작
+    }
+
+    //음성 인식 리스너
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            //사용자가 말하기 시작할 준비가 되면 호출된다
+            speechTextView.setText("음성인식을 시작합니다.");
+            speakTTS("음성인식을 시작합니다.");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            //사용자가 말하기 시작했을 때 호출된다
+            searchBtn.setTextColor(Color.YELLOW);
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            //입력받는 소리의 크기를 알려준다
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            //사용자가 말을 시작하고 인식이 된 단어를 buffer에 담는다
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            //사용자가 말하기를 중지하면 호출된다
+            searchBtn.setTextColor(Color.WHITE);
+        }
+
+        @Override
+        public void onError(int error) {
+            //네트워크 또는 인식 오류 발생 시 호출된다
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    speechTextView.setText("오디오 에러");
+                    speakTTS("오디오 에러가 발생했습니다.");
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    speechTextView.setText("클라이언트 에러");
+                    speakTTS("클라이언트 에러가 발생했습니다.");
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    speechTextView.setText("퍼미션 없음");
+                    speakTTS("권한이 없습니다.");
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    speechTextView.setText("네트워크 에러");
+                    speakTTS("네트워크 에러가 발생했습니다.");
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    speechTextView.setText("네트워크 시간초과");
+                    speakTTS("네트워크 시간이 초과되었습니다.");
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    speechTextView.setText("찾을 수 없음");
+                    speakTTS("찾을 수 없습니다");
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    speechTextView.setText("recognizer 바쁨");
+                    speakTTS("recognizer 바쁨");
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    speechTextView.setText("서버 에러");
+                    speakTTS("서버 에러가 발생했습니다.");
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    speechTextView.setText("말하기 시간초과");
+                    speakTTS("말하기 시간을 초과했습니다.");
+                    break;
+                default:
+                    speechTextView.setText("알 수 없는 에러 발생");
+                    speakTTS("알 수 없는 에러가 발생했습니다.");
+                    break;
+            }
+
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            //인식 결과가 준비되면 호출된다.
+            ArrayList<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for(int i = 0; i < matches.size() ; i++){
+                speechTextView.setText(matches.get(i));
+            }
+        }
+
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            //부분 인식 결과를 사용할 수 있을 때 호출된다
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            //향후 이벤트 추가하기 위해 예약된다
+        }
+    };
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        key = SpeechRecognizer.RESULTS_RECOGNITION;
+        speechTextView.setText(key);
+    }
+
 }
